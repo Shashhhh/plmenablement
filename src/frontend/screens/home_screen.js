9c70c5db9716799630d14aaf60da9175304e864c
Jason Lin <linjay@umich.edu>
2024-06-25 15:07:50 -0400

    Added background color to header

--- src/frontend/screens/home_screen.js
+++ src/frontend/screens/home_screen.js
@@ -4,8 +4,10 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
+      <View style = {styles.headerStyle}>
       <Text style={styles.welcomeText}>Welcome to Siemens GPT Trainer!</Text>
       <Text style={styles.helperText}>This is an unofficial Siemens app that is here to help answer any questions you have about blah blah blah, click any of the buttons below to continue!</Text>
+      </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.buttonAlign} onPress={() => navigation.navigate('Machinist GPT')}>
           <Text style={styles.buttonText}>Machinist GPT</Text>
@@ -24,18 +26,15 @@ export default function HomeScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    paddingVertical: 30,
-    paddingHorizontal: 10,
   },
   welcomeText: {
     fontSize: 25,
     fontWeight: 'bold',
+    color: 'white',
   },
   helperText: {
     paddingVertical: 5,
-    color: 'grey',
+    color: 'white',
     marginBottom: 10,
   },
   buttonContainer: {
@@ -57,5 +56,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 30,
     color: '#fff', 
+  },
+  headerStyle:
+  {
+    paddingTop: 30,
+    paddingBottom: 5,
+    paddingHorizontal: 10,
+    backgroundColor: '#009999',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    marginBottom: 20,
   },
 });
