4c6581747c93274f731c27725da6775bc8c31d26
Jason Lin <linjay@umich.edu>
2024-06-27 14:40:22 -0400

    Fixed thread creation
    Issue: each time an user sends a text, a new thread would be created, making it so it seemed like a new conversation everytime and it wouldn't remember what was previously asked.
    
    Changed it so that creates a thread on connection then just uses that thread during the entire connection

--- src/myapp/consumers.py
+++ src/myapp/consumers.py
@@ -8,7 +8,8 @@ class Handler(AsyncWebsocketConsumer):
     async def connect(self):
         self.client = AsyncOpenAI(api_key=settings.OPENAI_API_KEY)
         self.assistant_id = "asst_Mn4aCCQPzF5dJJD80Paq5uRU"
-        self.thread = self.client.beta.threads.create()
+        self.thread = await self.client.beta.threads.create()
+
         await self.accept()
 
     async def disconnect(self, close_code):
@@ -16,16 +17,16 @@ class Handler(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         user_input = text_data.strip()
-
-        stream = await self.client.beta.threads.create_and_run(
+        message = await self.client.beta.threads.messages.create(
+                thread_id=self.thread.id,
+                role="user",
+                content= user_input
+                )
+        stream = await self.client.beta.threads.runs.create(
                 assistant_id="asst_Mn4aCCQPzF5dJJD80Paq5uRU",
-                thread={
-                    "messages": [
-                        {"role": "user", "content": user_input}
-                    ]
-                },
+                thread_id=self.thread.id,
                 stream=True
-            )
+                )
         async for chunk in stream:
             if isinstance(chunk, ThreadMessageDelta):
                 await self.send(text_data=json.dumps({'delta': chunk.data.delta.content[0].text.value}))
