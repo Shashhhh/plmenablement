c33ffbb366740bb94bb91cb05cdf12fc42d40642
Jason Lin <linjay@umich.edu>
2024-08-17 22:18:27 -0400

    reworked ui

--- src/plm_enablement/src/screens/home_screen.js
+++ src/plm_enablement/src/screens/home_screen.js
@@ -7,26 +7,26 @@ export default function HomeScreen() {
 
   return (
     <div className='App'>
-      <div className="container">
-        <div className="textContainer">
-          <div className="welcomeText">Sales</div>
-          <div className="welcomeText">Enablement</div>
-          <div className="aboutText">about</div>
-          <div className="titleText">Tool and Die Shops</div>
-        <div className="helperText">
-          This simulator will help you learn about prospects and customers in the tool and die industry. Select one of the simulators below to get started!
+      <div className="homeContainer">
+        <div className="headerContainer">
+          <h1 className="welcomeText">Sales Enablement for Tool and Die Shops</h1>  
         </div>
-      </div>
+        <div className="helperContainer">
+          <h5 className="helperText">
+            This simulator will help you learn about prospects and customers in the tool and die industry. Select one of the simulators below to get started!
+          </h5>
+        </div>
+  
       <div className="bodyContainer">
       <div>
-      <button className="MachinistButton" onClick={() => navigate('/chat?assistantChoice=Machinist')}>
-          Learn About the Industry
-        </button>
-        <button className="DiscoveryButton" onClick={() => navigate('/chat?assistantChoice=Discovery')}>
-          Practice your Discovery
-        </button>
-        <button className="SalesButton" onClick={() => navigate('/chat?assistantChoice=Sales_call')}>
-          Simulate a Sales Call
+        <button className="MachinistButton" onClick={() => navigate('/chat?assistantChoice=Machinist')}>
+            Learn About the Industry
+          </button>
+          <button className="DiscoveryButton" onClick={() => navigate('/chat?assistantChoice=Discovery')}>
+            Practice your Discovery
+          </button>
+          <button className="SalesButton" onClick={() => navigate('/chat?assistantChoice=Sales_call')}>
+            Simulate a Sales Call
         </button>
       </div>
 
